a1e395d302869a4e108c8b559b1ccaaf3512fd1c
Dirk Thomas <dirk-thomas@users.noreply.github.com>
2020-10-14 09:15:04 -0700

    Rebase from 'debian/kinetic/catkin'

--- cmake/order_paths.py
+++ cmake/order_paths.py
@@ -1,26 +1,34 @@
 from __future__ import print_function
 
-from argparse import ArgumentParser
 import os
 import sys
+from argparse import ArgumentParser
 
+try:
+    from catkin_pkg.workspaces import get_spaces
+except ImportError as e:
+    sys.exit('ImportError: "catkin_pkg.workspaces import get_spaces" failed: %s\nMake sure that you have installed "catkin_pkg", it is up to date and on the PYTHONPATH.' % e)
 try:
     from catkin_pkg.workspaces import order_paths
 except ImportError as e:
-    sys.exit('ImportError: "from catkin_pkg.package import parse_package" failed: %s\nMake sure that you have installed "catkin_pkg", it is up to date and on the PYTHONPATH.' % e)
+    sys.exit('ImportError: "from catkin_pkg.workspaces import order_paths" failed: %s\nMake sure that you have installed "catkin_pkg", it is up to date and on the PYTHONPATH.' % e)
 
 
 def main():
-    """
-    Order a list of paths according to a list of prefixes which define the order.
-    """
+    """Order a list of paths according to a list of prefixes which define the order."""
     parser = ArgumentParser(description='Utility to order a list of paths according to a list of prefixes. Creates a file with CMake set command setting a variable.')
     parser.add_argument('outfile', help='The filename of the generated CMake file')
     parser.add_argument('--paths-to-order', nargs='*', help='The semicolon-separated paths to order')
     parser.add_argument('--prefixes', nargs='*', help='The semicolon-separated prefixes defining the order')
     args = parser.parse_args()
 
-    ordered_paths = order_paths(args.paths_to_order, args.prefixes)
+    # resolve the source space if any
+    spaces = []
+    for prefix in args.prefixes:
+        spaces.append(prefix)
+        spaces += get_spaces([prefix])
+
+    ordered_paths = order_paths(args.paths_to_order, spaces)
 
     # create directory if necessary
     outdir = os.path.dirname(args.outfile)
